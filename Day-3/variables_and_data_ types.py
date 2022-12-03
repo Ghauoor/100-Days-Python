57b81e2701d60ec52d47b8d37a7ceda104594e3a
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-03 21:48:28 +0500

    Day-3-Done

---
+++ Day-3/variables_and_data_ types.py
@@ -0,0 +1,16 @@
+a = 1
+print(type(a))
+b = "1"
+print(type(b))
+d = None
+
+list = [3, 4, 5, 3, 2, "Naruto"]
+
+print(list)
+
+tuple = ("Naruto", "Hinata", "Himawari", "Hinabi")
+
+print(tuple)
+
+dict = {"Name": "Naruto", "Age": 234, "CanVote": True}
+print(dict)
